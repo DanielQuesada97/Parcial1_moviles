ca2afe1f22da94bd8a03a547d935cd5212a2855f
Daniel Quesada <danielquesada@Daniels-MacBook-Pro.local>
2023-05-23 10:55:56 -0600

    final commit

--- app/src/main/java/com/quesada/parquetracker/ParkApplication.kt
+++ app/src/main/java/com/quesada/parquetracker/ParkApplication.kt
@@ -1,9 +1,10 @@
 package com.quesada.parquetracker
 
+import android.app.Application
 import com.quesada.parquetracker.data.parks
 import com.quesada.parquetracker.repositories.ParkRepository
 
-class ParkApplication {
+class ParkApplication : Application() {
     val parkRepository: ParkRepository by lazy {
         ParkRepository(parks)
     }
--- app/src/main/java/com/quesada/parquetracker/data/DummyData.kt
+++ app/src/main/java/com/quesada/parquetracker/data/DummyData.kt
@@ -3,7 +3,7 @@ package com.quesada.parquetracker.data
 import com.quesada.parquetracker.data.model.ParkModel
 
 val name = "Location 1"
-val location = "This is the location of Park 1"
+val location = "Location"
 val description = "This is the description of Park 1, it's a nice park"
 
 val parks = mutableListOf(
